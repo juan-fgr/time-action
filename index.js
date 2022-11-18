2b17372a40e105ab3ba8ac74626738f71cbaeddb
juan-fgr <juan.gonzalez@code.seat>
2022-11-18 18:01:39 +0000

    First commit, action to manage time

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const dayjs = require('dayjs');
+const dayjsPluginUTC = require('dayjs/plugin/utc')
+
+dayjs.extend(dayjsPluginUTC)
+
+async function main() {
+	try {
+		const timezone = core.getInput('timeZone');// default: 0
+		const formatStr = core.getInput('format');// default: ''
+		console.log('time zone: ', timezone)
+		console.log('time format: ', formatStr)
+
+		const str = dayjs().utcOffset(timezone).format(formatStr)
+		console.log("time formatStr: ", str)
+
+		core.setOutput("time", str);
+		
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+main();
