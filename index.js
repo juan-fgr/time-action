459b2c99d28e782490ac64e98e2dc0b4fbf99406
juan-fgr <juan.gonzalez@code.seat>
2022-11-21 17:34:50 +0000

    Updated files

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ dayjs.extend(dayjsPluginUTC)
 
 async function main() {
 	try {
+		if (core.getInput('format') !== '') {
+			manageTime();
+		}
+		if(core.getInput('string') !== ''){
+			manageString();
+		}
+		
+
+
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+main();
+
+
+
+function manageTime(){
 		const timezone = core.getInput('timeZone');// default: 0
 		const formatStr = core.getInput('format');// default: ''
 		console.log('time zone: ', timezone)
@@ -16,10 +35,21 @@ async function main() {
 		console.log("time formatStr: ", str)
 
 		core.setOutput("time", str);
-
-	} catch (error) {
-		core.setFailed(error.message);
-	}
 }
 
-main();
+function manageString(){
+		const inputStr = core.getInput('string');
+		console.log(`Manipulating string: ${inputStr}`);
+
+		const lowercase = inputStr.toLowerCase();
+		console.log(`lowercase: ${lowercase}`);
+		core.setOutput("lowercase", lowercase);
+
+		const uppercase = inputStr.toUpperCase();
+		console.log(`uppercase: ${uppercase}`);
+		core.setOutput("uppercase", uppercase);
+
+		const capitalized = inputStr.charAt(0).toUpperCase() + inputStr.slice(1).toLowerCase();
+		console.log(`capitalized: ${capitalized}`);
+		core.setOutput("capitalized", capitalized);
+}
